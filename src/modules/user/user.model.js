5b3961065012e0ce3e390ebd36dc578bf281ee6f
Eirenne <georgewiklauri@gmail.com>
2019-04-18 10:47:18 +0400

    feat: hash user password

--- src/modules/user/user.model.js
+++ src/modules/user/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -14,6 +15,42 @@ const userSchema = new mongoose.Schema({
     default: Date.now
 }
 });
+
+
+/*
+  Hash Password before saving
+*/
+userSchema.pre('save', function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const user = this;
+    bcrypt.hash(user.password, parseInt(process.env.SALT_ROUNDS),
+      function(err, hashedPassword) {
+      if (err) {
+        next(err);
+      }
+      else {
+        user.password = hashedPassword;
+        next();
+      }
+    });
+  } else {
+    next();
+  }
+});
+
+
+/*
+  Checks if provided password matches in the database
+*/
+userSchema.methods.isCorrectPassword = function(password, callback){
+  bcrypt.compare(password, this.password, function(err, same) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(err, same);
+    }
+  });
+}
 
 const User = mongoose.model('User', userSchema);
 
